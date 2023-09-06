a3ba9e3c3b4979666d0debdd62fa21d93cbe0b03
VoVanDuc20110635 <20110635@student.hcmute.edu.vn>
2023-09-06 08:13:40 +0700

    video8 routing and MVC

---
+++ src/route/web.js
@@ -0,0 +1,12 @@
+import express from "express";
+import homeController from '../controller/homeController'
+let router= express.Router();
+const initWebRoute = (app) =>{
+    router.get('/', homeController.getHomepage);
+    router.get('/about', (req, res) => {
+        res.send(`I'm Eric`);
+      })
+    return app.use('/', router);
+}
+// module.export = initWebRoute;
+export default initWebRoute;
--- src/server.js
+++ src/server.js
@@ -10,13 +10,21 @@
 //     console.log('NodeJs server is running on port: 3000');
 // })
 
-const express = require('express')
-const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// const express = require('express')
+import express from 'express';
+import configViewEngine from './configs/viewEngine';
+import initWebRoute from './route/web';
+require('dotenv').config();
+const app = express()
+const port = process.env.PORT || 8080;
+// the method backup, if process.env.PORT is undefined, port will equals 8080
+
+// set up viewEngine
+configViewEngine(app);
+
+// init web route
+initWebRoute(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
