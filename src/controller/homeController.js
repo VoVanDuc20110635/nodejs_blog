2ce4e5a3b564155ed2d08ee37f3efa1dd02eb8e8
VoVanDuc20110635 <20110635@student.hcmute.edu.vn>
2023-09-07 12:06:01 +0700

    video 13

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -20,8 +20,33 @@ let createNewUser = async(req, res) => {
     return res.redirect('/');
 }
 
+let deleteUser = async(req, res) =>{
+    let userId = req.body.userId;
+    await pool.execute('delete from users where id = ?', [userId]);
+    return res.redirect('/'); 
+}
+
+let getEditUser = async(req, res) => {
+    // console.log('edit user id: ', req.params.id);
+    let id = req.params.id;
+    let [user]  = await pool.execute(`select * from users where id  = ?`, [id]);
+    return res.render('update.ejs', {dataUser: user[0]}); 
+}
+
+let postUpdateUser = async (req, res) =>{
+    let {userId, firstName, lastName, email, address } = req.body;
+    console.log(firstName, lastName, email, address, userId);
+    await pool.execute(`update users set firstName = ?, lastName= ?, email = ?, address = ? where id = ?`, 
+        [firstName, lastName, email, address, userId]);
+    // return res.redirect(`/edit-user/${userId}`);
+    return res.redirect('/');
+}
+
 module.exports = {
     getHomepage,
     getDetailPage,
-    createNewUser
+    createNewUser,
+    deleteUser,
+    getEditUser,
+    postUpdateUser
 }
--- src/route/web.js
+++ src/route/web.js
@@ -5,6 +5,9 @@ const initWebRoute = (app) =>{
     router.get('/', homeController.getHomepage);
     router.get('/detail/user/:id', homeController.getDetailPage);
     router.post('/create-new-user', homeController.createNewUser);
+    router.post('/delete-user', homeController.deleteUser);
+    router.get('/edit-user/:id', homeController.getEditUser);
+    router.post('/update-user', homeController.postUpdateUser);
     router.get('/about', (req, res) => {
         res.send(`I'm Eric`);
       })
